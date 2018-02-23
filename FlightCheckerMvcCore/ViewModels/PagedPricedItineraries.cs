c70e408a7744dd7a1616c25ffac8fd148d9d41b2
iainkiloh <36046209+iainkiloh@users.noreply.github.com>
2018-02-23 12:51:02 +0000

    Add files via upload

---
+++ FlightCheckerMvcCore/ViewModels/PagedPricedItineraries.cs
@@ -0,0 +1,11 @@
+﻿using FlightChecker.Domain.Models;
+using System.Collections.Generic;
+
+namespace FlightCheckerMvcCore.ViewModels
+{
+    public class PagedPricedItineraries
+    {
+        public IEnumerable<PricedItinerary> PricedItineraries { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
---
+++ FlightCheckerMvcCore/ViewModels/PagingInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FlightCheckerMvcCore.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int PageSize { get; set; }
+     
+
+        public int PageNumber { get; set; }
+
+        //public int TotalPages { get; set; }
+
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / PageSize); }
+        }
+
+    }
+
+}
